4588156a82ba08b752e7939e4c12eb771c8a7f4b
John Crawford <coffeeboi47@protonmail.com>
2023-10-17 08:19:01 -0600

    aod: lower voltage going to the display

--- src/drivers/St7789.cpp
+++ src/drivers/St7789.cpp
@@ -23,6 +23,7 @@ void St7789::Init() {
 #endif
   NormalModeOn();
   SetVdv();
+  PowerControl();
   DisplayOn();
 }
 
@@ -175,6 +176,18 @@ void St7789::FrameRateNormal() {
 
 void St7789::DisplayOn() {
   WriteCommand(static_cast<uint8_t>(Commands::DisplayOn));
+}
+
+void St7789::PowerControl() {
+  WriteCommand(static_cast<uint8_t>(Commands::PowerControl1));
+  // Constant
+  WriteData(0xa4);
+  // Lowest possible voltages
+  WriteData(0x00);
+
+  WriteCommand(static_cast<uint8_t>(Commands::PowerControl2));
+  // Lowest possible boost circuit clocks
+  WriteData(0xb3);
 }
 
 void St7789::SetAddrWindow(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1) {
