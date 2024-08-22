ebe20c211720100b7f519a0964b5707ef67128bc
mark9064 <30447455+mark9064@users.noreply.github.com>
2024-08-22 16:53:18 +0100

    Clear ongoing taps when going to sleep

--- src/displayapp/DisplayApp.cpp
+++ src/displayapp/DisplayApp.cpp
@@ -314,6 +314,10 @@ void DisplayApp::Refresh() {
         } else {
           lcd.Sleep();
         }
+        // Clear any ongoing touch pressed events
+        // Without this LVGL gets stuck in the pressed state and will keep refreshing the
+        // display activity timer causing the screen to never sleep after timeout
+        lvgl.ClearTouchState();
         PushMessageToSystemTask(Pinetime::System::Messages::OnDisplayTaskSleeping);
         state = States::Idle;
         break;
